5f393aff8111759277ed708767e76b59c7877641
GagoPalyan <134396565+GagoPalyan@users.noreply.github.com>
2023-07-07 01:46:50 +0400

    the basket is ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,73 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div style={{ display: "none" }} className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30 d-flex justify-between">
+            Basket{" "}
+            <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
+          </h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{
+                  backgroundImage:
+                    "url(https://cdn.shopify.com/s/files/1/0049/9112/products/JordanBrandAIRJORDAN1RETROHIOGVARSITYMAIZEGAMEROYAL-SAIL7FD2596-700_1_2048x2048.jpg?v=1682086199)",
+                }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mr-5">WOMEN'S AIR JORDAN 1 RETRO HI OG</p>
+                <b>180.00$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{
+                  backgroundImage:
+                    "url(https://cdn.shopify.com/s/files/1/0049/9112/products/JordanBrandAIRJORDAN1RETROHIOGVARSITYMAIZEGAMEROYAL-SAIL7FD2596-700_1_2048x2048.jpg?v=1682086199)",
+                }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mr-5">WOMEN'S AIR JORDAN 1 RETRO HI OG</p>
+                <b>180.00$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Total:</span>
+                <div></div>
+                <b>360.00USD</b>
+              </li>
+              <li className="d-flex">
+                <span>Tax 5%:</span>
+                <div></div>
+                <b>18.00USD</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              checkout <img src="\img\arrow.svg" alt="arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img
@@ -33,9 +100,19 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">All sneakers</h1>
+        <div className="mb-40 d-flex justify-between align-center">
+          <h1>All sneakers</h1>
+          <div className="search-block d-flex">
+            <img src="\img\search.svg" alt="search"></img>
+            <input placeholder="Search..." />
+          </div>
+        </div>
+
         <div className="d-flex">
           <div className="card">
+            <div className="favorite">
+              <img src="\img\unliked.svg" alt="unliked" />
+            </div>
             <img
               width={133}
               height={112}
