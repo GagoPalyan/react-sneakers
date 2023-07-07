6cc5cc2695ed93189748e4de0deb1f2e7312d620
GagoPalyan <134396565+GagoPalyan@users.noreply.github.com>
2023-07-07 16:56:52 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,76 +1,11 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 function App() {
   return (
     <div className="wrapper clear">
-      <div style={{ display: "none" }} className="overlay">
-        <div className="drawer">
-          <h2 className="mb-30 d-flex justify-between">
-            Basket{" "}
-            <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
-          </h2>
-
-          <div className="items">
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{
-                  backgroundImage:
-                    "url(https://cdn.shopify.com/s/files/1/0049/9112/products/JordanBrandAIRJORDAN1RETROHIOGVARSITYMAIZEGAMEROYAL-SAIL7FD2596-700_1_2048x2048.jpg?v=1682086199)",
-                }}
-                className="cartItemImg"
-              ></div>
-
-              <div className="mr-20 flex">
-                <p className="mr-5">WOMEN'S AIR JORDAN 1 RETRO HI OG</p>
-                <b>180.00$</b>
-              </div>
-              <img
-                className="removeBtn"
-                src="/img/btn-remove.svg"
-                alt="Remove"
-              />
-            </div>
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{
-                  backgroundImage:
-                    "url(https://cdn.shopify.com/s/files/1/0049/9112/products/JordanBrandAIRJORDAN1RETROHIOGVARSITYMAIZEGAMEROYAL-SAIL7FD2596-700_1_2048x2048.jpg?v=1682086199)",
-                }}
-                className="cartItemImg"
-              ></div>
-
-              <div className="mr-20 flex">
-                <p className="mr-5">WOMEN'S AIR JORDAN 1 RETRO HI OG</p>
-                <b>180.00$</b>
-              </div>
-              <img
-                className="removeBtn"
-                src="/img/btn-remove.svg"
-                alt="Remove"
-              />
-            </div>
-          </div>
-          <div className="cartTotalBlock">
-            <ul>
-              <li>
-                <span>Total:</span>
-                <div></div>
-                <b>360.00USD</b>
-              </li>
-              <li className="d-flex">
-                <span>Tax 5%:</span>
-                <div></div>
-                <b>18.00USD</b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              checkout <img src="\img\arrow.svg" alt="arrow" />
-            </button>
-          </div>
-        </div>
-      </div>
-
+      <Drawer />
       <Header />
       <div className="content p-40">
         <div className="mb-40 d-flex justify-between align-center">
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -59,3 +59,5 @@ function Drawer() {
     </div>
   );
 }
+
+export default Drawer;
